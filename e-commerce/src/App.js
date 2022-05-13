904242748c78077ca2c30a02e881df878c3888a4
andershup <anders242@gmail.com>
2022-05-13 07:46:02 +0000

    update category details

---
+++ e-commerce/src/App.js
@@ -0,0 +1,43 @@
+const App = () => {
+  return (
+    <div className='categories-container'>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Tools</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Gifts</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Novelty Items</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Toys</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className='category-container'>
+        {/* <img /> */}
+        <div className='category-body-container'>
+          <h2>Bespoke</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
