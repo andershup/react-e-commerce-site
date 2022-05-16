34882e1e5df7e8d8b3b5cfc86477fc28ad2ec510
andershup <anders242@gmail.com>
2022-05-16 13:56:18 +0000

    add firebase config

---
+++ 3dprints/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVkKb7jtjupLlqoHSdXZMiF1unyofvPSY",
+  authDomain: "portfolio-react-site-d4951.firebaseapp.com",
+  projectId: "portfolio-react-site-d4951",
+  storageBucket: "portfolio-react-site-d4951.appspot.com",
+  messagingSenderId: "145535152411",
+  appId: "1:145535152411:web:c588538a6ee7b7ef4b23be"
+};
+
+  const firebaseApp = initializeApp(firebaseConfig);
