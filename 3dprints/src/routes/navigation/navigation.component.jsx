09e8718e832fd207e5242373faa508cc94afc22c
andershup <anders242@gmail.com>
2022-05-25 15:40:38 +0000

    add CartProvider to index.js add cart context

--- 3dprints/src/routes/navigation/navigation.component.jsx
+++ 3dprints/src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 
 import { ReactComponent as Logo } from '../../assets/disc.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
@@ -10,7 +11,7 @@ import './navigation.styles.scss';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
-
+  const { isCartOpen } = useContext(CartContext);
   return (
     <Fragment>
       <div className='navigation'>
