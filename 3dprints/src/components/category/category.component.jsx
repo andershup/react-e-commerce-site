542603e017d86bd99de7247d495cbee20cae22e5
andershup <anders242@gmail.com>
2022-06-08 07:18:35 +0000

    add directory-item and styles. delete category-item

--- 3dprints/src/components/category/category.component.jsx
+++ 3dprints/src/components/category/category.component.jsx
@@ -1,7 +1,17 @@
-const Category = () => {
-    return (
-        <div className="category">test</div>
-    )
-}
+import { Routes, Route } from 'react-router-dom';
 
-export default Category
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
+
+import './shop.styles.scss';
+
+const Shop = () => {
+  return (
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
+    </Routes>
+  );
+};
+
+export default Shop;
